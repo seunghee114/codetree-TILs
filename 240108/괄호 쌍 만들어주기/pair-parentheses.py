f344ba654b20483f0ca735a1ba7bf05ff636de0a
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-08 14:14:24 +0900

    Title: 괄호 쌍 만들어주기, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240108/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240108/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,15 +1,37 @@
 a = input()
-cnt = [0, 0]
+value = ord('(')
 num = [0, 0]    # (의 개수, )의 개수
 prev = a[0]
+acc = []
+cnt = [(0, 0)]
 for ch in a:
+    ch_idx = ord(ch)-value
     if prev == ch:
-        num[ord(ch)-ord('(')]+=1
+        num[ch_idx]+=1
     else:
-        cnt[ord(prev) - ord('(')] += num[ord(prev) - ord('(')]-1
-        num[ord(prev) - ord('(')] = 0
+        prev_idx = ord(prev) - value
+        if num[prev_idx] > 1:
+            if prev == '(':
+                acc.append(num[prev_idx]-1)
+            else:
+                acc.append(-(num[prev_idx]-1))
+        num[prev_idx] = 0
         prev = ch
-        num[ord(prev) - ord('(')] = 1
-cnt[ord(prev) - ord('(')] += num[ord(prev) - ord('(')]-1
+        num[ch_idx] = 1
 
-print(cnt[0] * cnt[1])
+prev_idx = ord(prev) - value
+if num[prev_idx] > 1:
+    if prev == '(':
+        acc.append(num[prev_idx]-1)
+    else:
+        acc.append(-(num[prev_idx]-1))      
+
+answer = 0
+neg = 0
+for i in range(len(acc)-1, -1, -1):
+    if acc[i] > 0:
+        answer += acc[i] * neg
+    else:
+        neg += -acc[i]
+
+print(answer)
