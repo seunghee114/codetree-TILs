42108ed848df9f9b6cec46f72427ad2bd9627bb6
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-21 22:06:07 +0900

    Title: 벽이 있는 충돌 실험, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240121/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 240121/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -1,28 +1,34 @@
-def get_bead(N, Board):
+def get_bead(N, Board, visit):
     beads = []
+    cnt = 0
     for i in range(N):
         for j in range(N):
             if len(Board[i][j]) == 0 or len(Board[i][j]) > 1:
                 continue
+            cnt+=1
+            if visit[i][j][Board[i][j][0]]:
+                continue
             beads.append([i, j, Board[i][j][0]])
-    return beads
+            visit[i][j][Board[i][j][0]] = True
+    return beads, visit, cnt
 def notRange(i, j, N):
     return i < 0 or j < 0 or i >= N or j >= N;
-def main_process(N, Board):
+def main_process(N, Board, visit):
     di = [-1, 0, 1, 0]
     dj = [0, -1, 0, 1]
-    for _ in range(N):
-        beads = get_bead(N, Board)
-        Board = [[[] for _ in range(N)] for _ in range(N)]
-        for bead in beads:
-            ni = bead[0] + di[bead[2]]
-            nj = bead[1] + dj[bead[2]]
-            if notRange(ni, nj, N):
-                d = (bead[2] + 2) % 4
-                Board[bead[0]][bead[1]].append(d)
-                continue
-            Board[ni][nj].append(bead[2])
-    return Board
+    beads, visit, cnt = get_bead(N, Board, visit)
+    if len(beads) == 0:
+        return None, None, cnt
+    Board = [[[] for _ in range(N)] for _ in range(N)]
+    for bead in beads:
+        ni = bead[0] + di[bead[2]]
+        nj = bead[1] + dj[bead[2]]
+        if notRange(ni, nj, N):
+            d = (bead[2] + 2) % 4
+            Board[bead[0]][bead[1]].append(d)
+            continue
+        Board[ni][nj].append(bead[2])
+    return Board, visit, cnt
 def in_process():
     N, M = map(int, input().split())
     Board = [[[] for _ in range(N)] for _ in range(N)]
@@ -32,12 +38,14 @@ def in_process():
     return N, Board
 def process():
     N, Board = in_process()
+    visit = [[[False for _ in range(4)]for _ in range(N)] for _ in range(N)]
+    cnt = 0
     while True:
-        before = len(get_bead(N, Board))
-        Board = main_process(N, Board)
-        after = len(get_bead(N, Board))
-        if before == after:
-            return after
+        Board, visit, cnt = main_process(N, Board, visit)
+        if Board == None:
+            break
+    return cnt
+        
 def main():
     T = int(input())
     for _ in range(T):
