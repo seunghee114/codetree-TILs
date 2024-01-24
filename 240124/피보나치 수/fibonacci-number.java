d681b8ff9c80e847fa52a44af7663316ae96e7e3
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-24 10:56:26 +0900

    Title: 피보나치 수, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240124/피보나치 수/fibonacci-number.java
+++ 240124/피보나치 수/fibonacci-number.java
@@ -6,12 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(N));
+    }
+
+    static int fibonacci(int N) {
         int[] dp = new int[N + 1];
         dp[1] = 1;
-        dp[2] = 1;
+        if (N == 1) return dp[N];
+        dp[2] = 2;
         for (int i = 3; i <= N; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        System.out.println(dp[N]);
+        return dp[N];
     }
 }
