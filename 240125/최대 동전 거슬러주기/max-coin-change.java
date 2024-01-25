ba12bc7aa83288bca9fbead6c160d52d7b5e1bdc
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-25 19:21:29 +0900

    Title: 최대 동전 거슬러주기, Time: 139ms, Memory: 17MB, Status: Passed - CodeTree

--- 240125/최대 동전 거슬러주기/max-coin-change.java
+++ 240125/최대 동전 거슬러주기/max-coin-change.java
@@ -9,16 +9,19 @@ public class Main {
         int M = Integer.parseInt(inArr[1]);
         inArr = br.readLine().split(" ");
         int[] coins = new int[N];
+        int[] dp =new int[M+1];
         for(int i = 0; i < N; i++){
             coins[i] = Integer.parseInt(inArr[i]);
+            if(coins[i] <= M)
+                dp[coins[i]] = 1;
         }
+        Arrays.sort(coins);
         // end input
 
-        int[] dp =new int[M+1];
         for(int price = 1; price <= M; price++){
             for(Integer coin : coins){
                 int temp = price - coin;
-                if(temp < 0) continue;
+                if(temp <= 0 || dp[temp] == 0) continue;
                 dp[price] = Math.max(dp[price], dp[temp]+1);
             }
         }
