b8a3d56db6fe5281903df5a9c05bbf4bf4e7db6f
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-25 18:22:28 +0900

    Title: 동전 거슬러주기, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240125/동전 거슬러주기/coin-change.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inArr = br.readLine().split(" ");
+        int N = Integer.parseInt(inArr[0]);
+        int M = Integer.parseInt(inArr[1]);
+        int[] coins = new int[N];
+        inArr = br.readLine().split(" ");
+        for(int i = 0; i < N; i++){
+            coins[i] = Integer.parseInt(inArr[i]);
+        }
+        // end input
+
+        int[] dp = new int[M+1];    // dp[i] : i 금액을 만들기 위해 필요한 동전의 최소 개수
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[0] = 0;
+        for(int price = 1; price <= M; price++){
+            for(Integer coin : coins){
+                int temp = price - coin;    // coin 동전을 쓰기 전에 만들었던 금액
+                if(temp < 0) continue;      // 0보다 작으면 안돼
+                temp = dp[temp] + 1;        // temp 금액을 만들었을 때 사용한 동전 개수에 coin 동전 1개 추가 
+                dp[price] = Math.min(dp[price], temp); 
+            }
+        }
+        System.out.println(dp[M]);
+    }
+}
