a2adf013ccf02d684cdd6fa97272a89353e71356
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-28 17:20:15 +0900

    Title: 고대 보물 지도의 비밀, Time: 340ms, Memory: 23MB, Status: Passed - CodeTree

--- 240128/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
+++ 240128/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.java
@@ -16,24 +16,27 @@ public class Main {
         }
         // dp[i][j] : i번 숫자가 부분 수열의 마지막이고, j개의 음수가 부분 수열에 포함되어 있다.
         int[][] dp = new int[N + 1][K + 1];
-        for (int i = 0; i < N + 1; i++) {
+        for (int i = 1; i < N + 1; i++) {
             Arrays.fill(dp[i], Integer.MIN_VALUE);
         }
         dp[0][0] = 0;
         for (int i = 1; i < N + 1; i++) {
             for (int j = 0; j < K + 1; j++) {
-                if (number[i] > 0) {    // 양수
+                if (number[i] >= 0) {    // 양수
                     if (dp[i-1][j] != Integer.MIN_VALUE) {  // 현재값을 마지막으로 하는 부분 수열
                         dp[i][j] = dp[i - 1][j] + number[i];
                     }
-                    dp[i][0] = number[i];   // 현재값을 시작으로 하는 부분 수열
+                    if (dp[i][0] == Integer.MIN_VALUE) {
+                        dp[i][0] = number[i];   // 현재값을 시작으로 하는 부분 수열
+                    } else {
+                        dp[i][0] = Math.max(dp[i][0], number[i]);
+                    }
                 } else {    // 음수
-                    if (j-1 < 0 ) continue;
-                    if (dp[i-1][j-1] != Integer.MIN_VALUE) {    // 현재값을 마지막으로 하는 부분 수열
+                    if (j-1 >= 0 && dp[i-1][j-1] != Integer.MIN_VALUE) {    // 현재값을 마지막으로 하는 부분 수열
                         dp[i][j] = dp[i - 1][j - 1] + number[i];
                     }
                     if (dp[i][1] == Integer.MIN_VALUE) {    // 현재값을 시작으로 하는 부분 수열
-                        dp[i][1] = number[i];   
+                        dp[i][1] = number[i];
                     } else {
                         dp[i][1] = Math.max(dp[i][1], number[i]);
                     }
@@ -44,7 +47,7 @@ public class Main {
         int answer = Integer.MIN_VALUE;
         for (int i = 0; i < N + 1; i++) {
             for (int j = 0; j < K + 1; j++) {
-                if (i == 0 && j == 0) continue;
+                if (i == 0 ) continue;
                 answer = Math.max(answer, dp[i][j]);
             }
         }
