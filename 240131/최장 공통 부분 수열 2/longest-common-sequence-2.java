1259a4dddaefca943d34680b2a9962682115abc9
seunghee <43427305+seunghee114@users.noreply.github.com>
2024-01-31 11:20:06 +0900

    Title: 최장 공통 부분 수열 2, Time: 255ms, Memory: 38MB, Status: Passed - CodeTree

--- 240131/최장 공통 부분 수열 2/longest-common-sequence-2.java
+++ 240131/최장 공통 부분 수열 2/longest-common-sequence-2.java
@@ -9,14 +9,11 @@ public class Main {
             this.i = i;
             this.j = j;
         }
-        public String toString(){
-            return "len: " + this.len + ", ( " + i +", " + j + " )";
-        }
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String tempA = "-" + br.readLine();
-        String tempB = "-" + br.readLine();
+        String tempB = "+" + br.readLine();
         char[] a = tempA.toCharArray();
         char[] b = tempB.toCharArray();
         int lenA = a.length;
@@ -35,23 +32,22 @@ public class Main {
                 if(dp[i-1][j-1] != null){
                     dp[i][j] = new Tuple(dp[i-1][j-1].len, i-1, j-1);
                 }
-                if(dp[i][j] != null && a[i] == b[i]){
+                if(dp[i][j] != null && a[i] == b[j]){
                     dp[i][j].len++;
                     continue;
                 }
                 if(dp[i-1][j] != null){
                     if(dp[i][j] == null || dp[i][j].len < dp[i-1][j].len){
-                        dp[i][j] = new Tuple(dp[i-1][j].len, i-1, j);    
+                        dp[i][j] = new Tuple(dp[i-1][j].len, i-1, j);
                     }
                 }
                 if(dp[i][j-1] != null){
                     if(dp[i][j] == null || dp[i][j].len < dp[i][j-1].len){
                         dp[i][j] = new Tuple(dp[i][j-1].len, i, j-1);
-                    } 
+                    }
                 }
-                System.out.printf("(i, j) = ( %d, %d ), %s\n", i, j, dp[i][j].toString());
             }
-            
+
         }
         StringBuilder sb = new StringBuilder();
         Tuple tp = new Tuple(-1, lenA-1, lenB-1);
